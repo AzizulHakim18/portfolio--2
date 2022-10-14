d8265827783a0b3ab5b810308c50d50377cc4530
AzizulHakim18 <hakimrony18@gamil.com>
2022-10-15 00:55:07 +0600

    service section added

--- script.js
+++ script.js
@@ -9,6 +9,25 @@ function closeNav() {
     document.getElementById('backdrop').style.display = "none"
 }
 
+// about section button 
+function openCity(cityName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(cityName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
 
 // google chart js file
 google.charts.load('current', { 'packages': ['corechart'] });
